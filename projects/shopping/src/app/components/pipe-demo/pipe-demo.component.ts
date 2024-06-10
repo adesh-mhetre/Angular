f43459bb3524ef3d2513e0c62e40243f62a16fcb
Adesh Mhetre <adeshmhetre143@gmail.com>
2024-06-10 22:40:38 +0530

    Pipe Demo Component

--- projects/shopping/src/app/components/pipe-demo/pipe-demo.component.ts
+++ projects/shopping/src/app/components/pipe-demo/pipe-demo.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './pipe-demo.component.css'
 })
 export class PipeDemoComponent {
-
+  public Product:{Name:string, Price:number, Mfd:Date, Rate:number}={
+    Name: 'Samsung Tv',
+    Price: 34000.30,
+    Mfd: new Date("2023-01-22"),
+    Rate : 0.0245
+  }
 }
